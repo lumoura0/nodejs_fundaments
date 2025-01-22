c9d1ae6946cbd1a2b1dbcd1fe62b460c526cef52
Luis Fernando <luisfernando.mourag@gmail.com>
2025-01-22 17:30:31 -0300

    creating database local

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import http from "node:http"
+import { Database } from './database.js'
+import { json } from './middlewares/json.js'
 
 // Criar um usuário {name, email, senha}
 // Listagem de usuários
@@ -9,22 +11,32 @@ import http from "node:http"
 // HTTP
 // Método HTTP
 // URL
-const users = []
-const server = http.createServer((req, res) => {
+
+const database = new Database()
+
+
+const server = http.createServer(async (req, res) => {
     const { method, url } = req
 
+    await json(req, res)
+
     if (method === "GET" && url === "/users") {
+        const users = database.select('users')
+
         return res
-            .setHeader("Content-Type", "application/json")
             .end(JSON.stringify(users))
     }
 
     if (method === "POST" && url === "/users") {
-        users.push({
-            id: ,
-            name: "shiro",
-            email: "example@email.com"
-        })
+        const { name, email } = req.body
+
+        const user = {
+            id: 1,
+            name,
+            email,
+        }
+        database.insert('users', user)
+
         return res.writeHead(201).end()
     }
 
--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -3,20 +3,37 @@
 // process.stdin
 //     .pipe(process.stdout)
 
-import { Readable } from 'node:stream'
+import { Readable, Writable, Transform } from 'node:stream'
 
 class OneToHundredStream extends Readable {
     index = 1
     _read() {
         const i = this.index++
+        setTimeout(() => {
+            if (i > 100) {
+                this.push(null)
+            } else {
+                this.push(String(i) + '\n')
+            }
+        }, 1000);
+    }
+}
 
-        if (i > 100) {
-            this.push(null)
-        } else {
-            this.push(String(i) + '\n')
-        }
+class InverseNumberStream extends Transform {
+    _transform(chunk, encoding, callback) {
+        const transformed = Number(chunk.toString()) * -1
+
+        callback(null, Buffer.from(String(transformed)))
+    }
+}
+
+class multiplyByTenStream extends Writable {
+    _write(chunk, encoding, callback) {
+        console.log(Number(chunk.toString()) * 10)
+        callback()
     }
 }
 
 new OneToHundredStream()
-    .pipe(process.stdout)
+    .pipe(new InverseNumberStream())
+    .pipe(new multiplyByTenStream())
