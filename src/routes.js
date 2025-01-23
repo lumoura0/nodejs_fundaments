c477103ab22c8e4d8d93f179f08534e582861623
Luis Fernando Moura Goncalves <lfmgoncalves@emsurb.se.gov.br>
2025-01-23 16:39:37 -0300

    create methods http

---
+++ src/routes.js
@@ -0,0 +1,54 @@
+import { Database } from './database.js'
+import { randomUUID } from "node:crypto"
+import { buildRoutePath } from './utils/build-route-path.js'
+
+const database = new Database()
+
+// Query Parameters: URL Stateful => Filtros, paginação, não-obrigatórios
+// Route Parameters: Identificação de recurso
+// Ambos ficam na URL
+// Request Body: Envio de informação de um formulário (HTTPs)
+
+// http://localhost:3333/users?userId=1
+// GET http://localhost:3333/users/1
+// DELETE http://localhost:3333/users/1
+// POST http://localhost:3333/users
+
+// Edição e Remoção do usuário
+
+export const routes = [
+    {
+        method: 'GET',
+        path: buildRoutePath('/users'),
+        handler: (req, res) => {
+            const users = database.select('users')
+
+            return res.end(JSON.stringify(users))
+        }
+    },
+    {
+        method: 'POST',
+        path: buildRoutePath('/users'),
+        handler: (req, res) => {
+            const { name, email } = req.body;
+
+            const user = {
+                id: randomUUID(),
+                name,
+                email,
+            }
+            database.insert('users', user)
+
+            return res.writeHead(201).end()
+        }
+    },
+    {
+        method: 'DELETE',
+        path: buildRoutePath('/users/:id'),
+        handler: (req, res) => {
+            const { id } = req.params
+            database.delete('users', id)
+            return res.writeHead(204).end()
+        }
+    }
+]
---
+++ src/utils/build-route-path.js
@@ -0,0 +1,9 @@
+// Encontrar dentro dos parametros o que tem : -> '/users/:id
+export function buildRoutePath(path) {
+    const routeParametersRegex = /:([a-zA-Z]+)/g
+    const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)')
+
+    const pathRegex = new RegExp(`^${pathWithParams}`)
+
+    return pathRegex
+}
